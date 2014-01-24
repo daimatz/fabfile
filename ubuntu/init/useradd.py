2275226c9882eba03002074c71f662058e6ec141
daimatz <dai@daimatz.net>
2014-01-24 11:46:51 +0900

    make idempotent in useradd_ssh_config

--- ubuntu/init/useradd.py
+++ ubuntu/init/useradd.py
@@ -1,16 +1,21 @@
-from fabric.api import sudo, task, put
+from fabric.api import sudo, task, put, settings
 
 user = 'dai'
 github_user_id = 'daimatz'
 
+import os
+template_dir = os.path.dirname(__file__) + '/../template'
+
 @task
 def useradd_ssh_config():
     sudo('apt-get update')
-    sudo('apt-get install -y curl passwd')
-    sudo('echo 127.0.1.1 `hostname` >> /etc/hosts')
-    sudo('useradd %s -d /home/%s -m' % (user, user))
-    # sudo('echo %s:%s | chpasswd' % (user, user))
-    sudo('echo "%s ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/%s' % (user, user))
+    sudo('apt-get install -y curl')
+    with settings(warn_only=True):
+        if sudo('grep 127.0.1.1 /etc/hosts').failed:
+            sudo('echo 127.0.1.1 `hostname` >> /etc/hosts')
+        if sudo('grep ^%s /etc/passwd' % user).failed:
+            sudo('useradd %s -d /home/%s -m' % (user, user))
+    sudo('echo "%s ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/%s' % (user, user))
     sudo('chmod 440 /etc/sudoers.d/%s' % user)
     sudo('mkdir -p /home/%s/.ssh' % user)
     sudo('curl https://github.com/%s.keys > /home/%s/.ssh/authorized_keys'
@@ -18,5 +23,6 @@ def useradd_ssh_config():
     sudo('chmod 700 /home/%s/.ssh' % user)
     sudo('chmod 600 /home/%s/.ssh/authorized_keys' % user)
     sudo('chown -R %s:%s /home/%s/.ssh' % (user, user, user))
-    put('template/etc/ssh/sshd_config', '/etc/ssh/sshd_config', use_sudo=True)
+    put('%s/etc/ssh/sshd_config' % template_dir, '/etc/ssh/sshd_config',
+        use_sudo=True)
     sudo('service ssh restart')
