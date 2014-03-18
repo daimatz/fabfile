6962fb8dbeda3f9004e007ba013ff36dc55c36a0
daimatz <dai@daimatz.net>
2014-03-18 14:59:51 +0900

    remove unneeded docker

--- ubuntu/software/ruby.py
+++ ubuntu/software/ruby.py
@@ -2,8 +2,8 @@ from fabric.api import sudo, task, cd
 
 @task
 def install():
-    # with cd('/tmp'):
-    #     sudo('git clone https://github.com/ruby/ruby.git')
+    with cd('/tmp'):
+        sudo('git clone https://github.com/ruby/ruby.git')
     with cd('/tmp/ruby'):
         sudo('git checkout v2_1_0')
         sudo('autoconf')
